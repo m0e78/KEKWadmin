8ebf7edf91cac1c63baee61f3edfe148a96b5b73
moe <moezzanza@gmail.com>
2024-03-23 15:04:25 +0100

    redirects

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8800",
+      "/api": "https://kekwapi.onrender.com",
     },
   },
 })
